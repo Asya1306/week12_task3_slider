67a57b981e9ce91f94f202eb2e1672fdfdcc9913
Asya1306 <104448604+Asya1306@users.noreply.github.com>
2022-11-28 12:36:16 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+let picture = document.getElementById('img_code');
+
+let changeImgCode1 = () => {
+    picture.src = 'assets/img/code1.jpg';
+}
+
+let changeImgCode2 = () => {
+    picture.src = 'assets/img/code2.jpg';
+}
